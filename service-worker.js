eb7e6052491cf7f18fcc1fc43094ef0b70080dab
serinato-oracle <juju3355juju@gmail.com>
2026-09-09 00:59:45 +0900

    Show encyclopedia cards in four columns

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "kunimamori-pwa-20260908-25";
+const CACHE_NAME = "kunimamori-pwa-20260909-26";
 
 const CARD_IMAGES = Array.from({ length: 48 }, function (_, index) {
   return "./images/web/kuni-" + String(index + 1).padStart(2, "0") + ".jpg";
@@ -12,7 +12,7 @@ const APP_ASSETS = [
   "./manifest.webmanifest",
   "./manifest-en.webmanifest",
   "./style.css?v=20260903-4",
-  "./style.css?v=20260908-14",
+  "./style.css?v=20260909-15",
   "./terms/",
   "./privacy/",
   "./disclaimer/",
